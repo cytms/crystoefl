f5739e1d7b37eaaa2ec6984d9b0bf32e2531348f
Pinyen <b98705008@ntu.edu.tw>
2013-06-20 12:54:12 +0800

    select and translate vocabs in get_datas controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@
 class ArticlesController < ApplicationController
   require 'json'
   require 'stemmify'
+  require 'open-uri'
   #require 'kaminari'
   def index
     @articles = Article.page(params[:page]).per(100)
@@ -9,7 +10,7 @@ class ArticlesController < ApplicationController
     @array = []
     File.foreach("public/word_list.txt") do |line|
       @array.push(line.strip.stem)
-      puts @array.last
+      # puts @array.last
     end
 
 
@@ -27,21 +28,62 @@ class ArticlesController < ApplicationController
 
   def get_datas
   	puts "get_datas..."
+
+    # read vocabulary
+    @dictionary = []
+    File.foreach("public/word_list.txt") do |line|
+      @dictionary.push(line.strip.stem)
+      puts @dictionary.last
+    end
+
+    counter = 0 # for testing only
+
   	File.foreach('public/articles/Result.txt') do |line|
       file, category = line.split(' ')
       folder, tmp = file.split('_')
       File.foreach("public/articles/#{folder}/#{file}") do |content|
         json_file = JSON.parse(content)
+
+        article = json_file["content"]
+        @vocab_hash = {}
+        article.split(" ").each do |term|
+          stem_term = term.stem
+          if @dictionary.include?(stem_term)
+            @vocab_hash[term] = stem_term
+          end
+        end
+        
+        puts @vocab_hash
+
+        @vocab_with_translate = {}
+
+        @vocab_hash.each_key{|key|
+          @link = "http://tw.dictionary.yahoo.com/dictionary?p=" + key.to_s
+
+          doc = Nokogiri::HTML(open(@link))
+          @translate = doc.css('li.explanation_pos_wrapper')
+
+          @vocab_with_translate[key] = @translate.text # .text could be eliminated 
+
+        }
+
+        
+
         Article.create(:url => json_file["url"], :content => json_file["content"], :web_category => json_file["web_category"], :advice_category => json_file["advice_category"], :picture_url => json_file["picture_url"], :title => json_file["title"], :author => json_file["author"], :knn_category => category)
       end
 
+
+      counter = counter + 1 # for testing only
+      if counter > 6
+          break
+      end
       # stemming 
       # match 
       # translate
       # db: AddColumn text
 
     end
-  	redirect_to '/'
+  	# redirect_to '/'
   end
 
   def search
